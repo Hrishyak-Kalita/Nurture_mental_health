8a33c82a2c9bb4ef43408ae18ac3004609fd4b7f
kherkatarySan <sandeep21_ug@cse.nits.ac.in>
2024-02-17 13:09:52 +0530

    EDIT: removed news and login from navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React,{ useState, Component } from "react";
+import { useState } from "react";
 import "./Navbar.scss"
 import { IoMenu } from "react-icons/io5";
 import { Link } from "react-router-dom";
@@ -18,9 +18,9 @@ const Navbar = () => {
                <li><Link to="/" className="home-menu">Home</Link></li>
                <li><Link to="/about">About Us</Link></li>
                <li><Link to="/services">Services</Link></li>
-               <li> <Link to="/news">Blogs</Link></li>
+               {/* <li> <Link to="/news">Blogs</Link></li> */}
                <li><Link to="/contact">Contact</Link></li>
-               <li><Link to="/login">Log In</Link></li>
+               {/* <li><Link to="/login">Log In</Link></li> */}
 
             </ul>
          </nav>
@@ -35,9 +35,9 @@ const Navbar = () => {
                <li><Link to="/" className="home-menu">Home</Link></li>
                <li><Link to="/about">About Us</Link></li>
                <li><Link to="/services">Services</Link></li>
-               <li> <Link to="/news">Blogs</Link></li>
+               {/* <li> <Link to="/news">Blogs</Link></li> */}
                <li><Link to="/contact">Contact</Link></li>
-               <li><Link to="/login">Log In</Link></li>
+               {/* <li><Link to="/login">Log In</Link></li> */}
 
                </ul>
             </div> 
