7c0a191a84dab65d355a84db5a020092efd16452
kherkatarySan <sandeep21_ug@cse.nits.ac.in>
2024-02-17 16:29:36 +0530

    ADD call button testing

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./Home.scss"
 import { Link } from 'react-router-dom'
-import { Button ,Navbar,Testimonial, Appointment_img_icon, ConnectMe} from '../../Components'
+import { Button ,Navbar,Testimonial, Appointment_img_icon, ConnectMe, ConnectCall} from '../../Components'
 import Carousel from '../../Components/Carousel/Carousel'
 import IntroToAdd from '../../Components/IntroToAdd/IntroToAdd'
 import MedicaIntro from '../../Components/MedicaIntro/MedicaIntro'
@@ -14,6 +14,7 @@ const Home = () => {
   return (
     <div className='home'>
       <ConnectMe phone="9706245997" size='30'/>
+      <ConnectCall size='30'/>
       <Carousel />
       <IntroToAdd />
       <MedicaIntro />
