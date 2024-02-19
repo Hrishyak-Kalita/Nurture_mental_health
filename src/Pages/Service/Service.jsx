2119d9c8fe437325c05314d4f48e58a1d2e188ec
indraray <techyedutech@gmail.com>
2024-02-20 00:50:08 +0530

    check

--- src/Pages/Service/Service.jsx
+++ src/Pages/Service/Service.jsx
@@ -8,7 +8,7 @@ const Service = () => {
 
   return (
     <>
-      <TileDesc name="OUR SERVICES" description="SERVICES"/>
+      {/* <TileDesc name="OUR SERVICES" description="SERVICES"/> */}
       {/* <TileDesc name="OUR SERVICES" /> */}
       <Quote heading="Our Services" subHeading="Get the best Therapy and Counselling"/>
 
