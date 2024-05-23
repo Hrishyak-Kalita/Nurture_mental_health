53503e3d38e36bcff39f4b32573c8d5ce6ed2f83
Hrishyak Kalita <hrishyakkalita@gmail.com>
2024-05-24 00:12:52 +0530

    corrections

--- src/Components/IntroToAdd/IntroToAdd.jsx
+++ src/Components/IntroToAdd/IntroToAdd.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import "./IntroToAdd.scss";
 
 const IntroToAdd = ({ phoneNumber, message }) => {
-    const handleWhatsAppGroup = () => {
+  const handleWhatsAppGroup = () => {
     const whatsappURL = `https://chat.whatsapp.com/C8Bx6VQkhP61Nbe1K73WyL`;
-    
-    window.open(whatsappURL, "_blank");
-    };
 
-    return (
+    window.open(whatsappURL, "_blank");
+  };
+
+  return (
     <div className="IntroToAddbox">
-        <button className="JoinBtn" onClick={handleWhatsAppGroup}>Join our Free Workshops</button>
-        <span className="discount"> 50% OFF for Students</span>
+      <button className="JoinBtn" onClick={handleWhatsAppGroup}>
+        Join our Free Workshops
+      </button>
+      <span className="discount"> 50% OFF for Students in Therapy</span>
     </div>
-    );
+  );
 };
 
 export default IntroToAdd;
